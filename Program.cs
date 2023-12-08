401461fcb3f5293d28252c2b89a1b248d9f65ad9
drlellinger <39723641+drlellinger@users.noreply.github.com>
2023-12-08 10:24:55 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,7 +9,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+//API-Dokumentation
+builder.Services.AddSwaggerGen(action => {
+
+var xmlFile = "Vertragsmanagement.xml";
+var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+action.IncludeXmlComments(xmlPath);
+});
 
 builder.Services.AddDbContext<Vertragsmanagement.DataAccess.DatabaseContext>(options =>
 {
